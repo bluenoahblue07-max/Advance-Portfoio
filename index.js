a97018e6d54a3de92529b681680c19200779c8d8
bluenoahblue07-max <bluenoahblue07@gmail.com>
2026-06-08 17:01:48 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+let isModalOpen = false;
+let contrastToggle = false;
+
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+      document.body.classList += " dark-theme"
+}  
+else {
+document.body.classList.remove("dark-theme")
+     }
+ }
+
+
+function contact(event) {
+   event.preventDefault();
+   const loading = document.querySelector('.modal__overlay--loading'); 
+   const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible";
+     emailjs
+      .sendForm(
+        'service_h9pz1yr',
+         'template_q163blz',
+         event.target,
+         'T8wPDZkzeoKYpLgQx'
+      ).then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+      }).catch(() => {
+         loading.classList.remove("modal__overlay--visible");
+         alert(
+            "The email service is temporarily down, please contact me directly at bluenoahblue07@gmail.com"
+         );
+      })
+}
+
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open")
+       
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
+}
+
